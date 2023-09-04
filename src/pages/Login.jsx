adfcaaf35423bb1672703c81b08d95b4b2a72de3
Lex Smith <lex.smith@live.com>
2023-09-04 09:44:05 +0100

    adding UC

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import Users from "../components/Users";
+import { LogInDetails } from "../components/LogInDetails";
+const LogInPage = () => {
+  return (
+    <div>
+      {/* <p>You're logging in</p> */}
+      <LogInDetails />
+      {/* <Users /> */}
+    </div>
+  );
+};
+
+export default LogInPage;
