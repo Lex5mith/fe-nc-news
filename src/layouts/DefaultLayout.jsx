529b42482a0aef8b24fe3dbe16394abf3d6e71d4
Lex Smith <lex.smith@live.com>
2023-09-04 21:18:52 +0100

    added ArticleList to homepage

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,7 +1,11 @@
-import * as React from "react";
+import { useContext } from "react";
 import NavBar from "../components/NavBar";
 import Box from "@mui/material/Box";
+import { ModeContext } from "../contexts/Mode.jsx";
+
 export const DefaultLayout = ({ children }) => {
+  const { mode } = useContext(ModeContext);
+  console.log(mode.palette);
   return (
     <>
       <NavBar />
@@ -9,8 +13,10 @@ export const DefaultLayout = ({ children }) => {
         sx={{
           padding: "4em",
           display: "flex",
+          flexGrow: 1,
           flexDirection: "column",
           alignItems: "center",
+          backgroundColor: mode.palette.backgroundColor,
         }}
       >
         {children}
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,29 +1,30 @@
-
-import {createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 export const lightTheme = createTheme({
-    palette: {
-      mode: "light",
-      primary: {
-        main: "#90BE6D",
-      },
-      secondary: {
-        main: "#CBA135",
-      },
+  palette: {
+    mode: "light",
+    // background: {
+    //   default: "#cea996",
+    //   paper: "#cea996",
+    //   box: "#cea996",
+    // },
+    primary: {
+      main: "#90BE6D",
     },
-  });
-  
-  export const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-      primary: {
-        main: "#2E3138",
-      },
-      secondary: {
-        main: "#6C6F7D",
-      },
-      // info: {
-      //   main: '#E0AFA0',
-      // },
+    secondary: {
+      main: "#CBA135",
     },
-  });
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#2E3138",
+    },
+    secondary: {
+      main: "#6C6F7D",
+    },
+  },
+});
