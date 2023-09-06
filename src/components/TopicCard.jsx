bd5c70d5ee6514b5cf24ea861402d41756c8929b
Lex Smith <lex.smith@live.com>
2023-09-06 14:42:30 +0100

    wip sort and filter

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,23 +1,13 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import ThumbsUpDownIcon from "@mui/icons-material/ThumbsUpDown";
-import CommentIcon from "@mui/icons-material/Comment";
-import moment from "moment";
 
 export const TopicCard = ({ topic }) => {
   return (
-    <Link
-      to={`/topics/`}
-      style={{ textDecoration: "none" }}
-    >
+    <Link to={`/?topic=${topic.slug}`} style={{ textDecoration: "none" }}>
       <Card sx={{ minHeight: "25em" }}>
-       
         <CardContent>
           <Typography variant="h6" color="text.secondary">
             # {topic.slug}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,93 @@
-import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
 import { ArticleList } from "../components/ArticleList";
+import { SortBar } from "../components/SortBar";
 export const Home = () => {
-  return <ArticleList />;
+  /**
+   * query params
+   * query string
+   * react-router-dom
+   *
+   * assign topicFilter to state
+   */
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [topicFilter, setTopicFilter] = useState("");
+  const [dateSortDirection, setDateSortDirection] = useState("");
+  const [commentCountSortDirection, setCommentCountSortDirection] =
+    useState("");
+  const [votesSortDirection, setVotesSortDirection] = useState("");
+
+  const topicQuery = searchParams.get("topic");
+
+  const handleSortByDate = () => {
+    setCommentCountSortDirection("");
+    setVotesSortDirection("");
+    if (dateSortDirection === "") {
+      setDateSortDirection("asc");
+    }
+    if (dateSortDirection === "asc") {
+      setDateSortDirection("desc");
+    }
+    if (dateSortDirection === "desc") {
+      setDateSortDirection("asc");
+    }
+  };
+  const handleSortByCommentCount = () => {
+    setDateSortDirection("");
+    setVotesSortDirection("");
+    if (commentCountSortDirection === "") {
+      setCommentCountSortDirection("asc");
+    }
+    if (commentCountSortDirection === "asc") {
+      setCommentCountSortDirection("desc");
+    }
+    if (commentCountSortDirection === "desc") {
+      setCommentCountSortDirection("asc");
+    }
+  };
+  const handleSortByVotes = () => {
+    setDateSortDirection("");
+    setCommentCountSortDirection("");
+    if (votesSortDirection === "") {
+      setVotesSortDirection("asc");
+    }
+    if (votesSortDirection === "asc") {
+      setVotesSortDirection("desc");
+    }
+    if (votesSortDirection === "desc") {
+      setVotesSortDirection("asc");
+    }
+  };
+
+  console.log({
+    dateSortDirection,
+    votesSortDirection,
+    commentCountSortDirection,
+  });
+
+  useEffect(() => {
+    if (topicQuery) {
+      setTopicFilter(topicQuery);
+    }
+  }, [topicQuery]);
+
+  console.log(topicQuery);
+  return (
+    <>
+      <SortBar
+        handleSortByDate={handleSortByDate}
+        handleSortByCommentCount={handleSortByCommentCount}
+        handleSortByVotes={handleSortByVotes}
+        dateSortDirection={dateSortDirection}
+        commentCountSortDirection={commentCountSortDirection}
+        votesSortDirection={votesSortDirection}
+      />
+      <ArticleList
+        topicFilter={topicFilter}
+        dateSortDirection={dateSortDirection}
+        commentCountSortDirection={commentCountSortDirection}
+        votesSortDirection={votesSortDirection}
+      />
+    </>
+  );
 };
